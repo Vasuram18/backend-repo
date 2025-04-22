3b838d5d78b9b49bca85d6d2bbbac39b20d1826c
Vasuram <2022csb1150@iitrpr.ac.in>
2025-04-23 01:52:56 +0530

    ////

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 //TODO: Change the origin to deployed frontend URL
 app.use(cors({
-    origin: "https://backend-repo-production-27bd.up.railway.app",
+    origin: "https://backend-repo-production-27bd.up.railway.app/",
     credentials: true
 }));
 
